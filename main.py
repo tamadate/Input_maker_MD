30bcdc6d7fa769bf3475e48362ec0908c8932057
tama3rdGen <tamatomo.21@gmail.com>
2022-09-05 14:22:27 -0500

    Change file mapping

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import functions
 import output
 
 ##  Input parameters
-fileName="aminoAcid/tryptophanRESP"  # file name
+fileName="alcohol/input/EtOHRESP"  # file name
 L=300.0000      # calculation domain size (only of lammps input)
-outputName="tryptophan"
+outputName="EtOH"
 
 ##  Read potential file
 Atoms=[]
